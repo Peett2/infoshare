8600c1b0408688a7a55ae59814ecca9887a6e87c
Peett2 <piotr@skotnicki.co>
2018-01-31 20:11:37 +0100

    create variables

---
+++ day 2/variables.py
@@ -0,0 +1,19 @@
+age = 34                #int
+temperature = 8.2       #float
+name = 'jakis text'     #string
+programmer = True       #bool
+nothing = None          #None
+
+print(age)
+print(temperature)
+print(name)
+print(programmer)
+print('Mój wiek to:', age)
+print(type(age))
+
+# @TODO wyświetl zmienne na ekranie
+
+age = 52
+print(age)
+
+# @TODO wyświetl typy wszystkich zmiennych
