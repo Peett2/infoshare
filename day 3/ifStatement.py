43d7159596c589badc5df4fca6a2e9e8eb6e1fee
Peett2 <piotr@skotnicki.co>
2018-02-05 20:26:48 +0100

    text cleanup

---
+++ day 3/ifStatement.py
@@ -0,0 +1,26 @@
+a = 0
+b = 2.34
+text = 'abc'
+
+if b > a:
+    print('b > a')
+elif b == a:
+    print('b == a')
+else:
+    print('b < a')
+
+if text:
+    print ('text is not empty')
+
+#  @todo if 'a' < 'h' < 'z'
+
+is_rest_equal_to_zero = 4 % 2
+
+if not is_rest_equal_to_zero:
+    print('the number is even')
+
+name = 'paweł'
+lastname='kowalski'
+
+if name =='jan' or lastname=='kowalski':
+    print('siema!')
---
+++ day 3/task.py
@@ -0,0 +1,9 @@
+digit=int(input('podaj liczbe: \n'))
+print(type(digit),digit)
+
+# @todo sprawdzanie czy liczba jest parzysta
+# @todo
+    if is_even:
+        print('liczba jest parzysta')
+    else:
+        print('liczba nie jest parzysta')
