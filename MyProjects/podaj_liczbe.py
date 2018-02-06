d1eb1afda618511b611517119c2b4b8675971448
Peett2 <piotr@skotnicki.co>
2018-02-06 18:03:50 +0100

    text cleanup

---
+++ MyProjects/podaj_liczbe.py
@@ -0,0 +1,14 @@
+from random import randint
+
+number = int(input("Podaj dowolną liczbę od 0 do 99:\n"))
+
+random_number = randint(0,100)
+
+if len(str(number))>2:
+    print('Za duża liczba - oszukujesz')
+elif number < random_number:
+    print('Moja liczba to',random_number,'\nTwoja liczba jest mniejsza - PRZEGRYWASZ')
+elif number > random_number:
+    print('Moja liczba to',random_number,'\nTwoja liczba jest większa - WYGRYWASZ')
+else:
+    print('Mamy remis :)')
