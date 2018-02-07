264da22466c26ca1417a0f55cff03c1af031071b
Peett2 <piotr@skotnicki.co>
2018-02-07 17:46:57 +0100

    text cleanup

---
+++ MyProjects/boy_girl.py
@@ -0,0 +1,6 @@
+aa = str(input('Jak masz na imię?\n'))
+
+if aa.endswith('a') and aa!='Jan Maria':
+    print('Cześć Koleżanko',aa.capitalize())
+else:
+    print('Cześć Kolego',aa.capitalize())
--- day 3/testing.py
+++
@@ -1,6 +0,0 @@
-aa = str(input('Jak masz na imię?\n'))
-
-if aa.endswith('a') and aa!='Jan Maria':
-    print('Cześć Koleżanko',aa.capitalize())
-else:
-    print('Cześć Kolego',aa.capitalize())
