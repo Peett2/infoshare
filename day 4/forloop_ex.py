eaa618d478d4408568dc35c2fa8e6186aae22a56
Peett2 <piotr@skotnicki.co>
2018-02-08 17:27:56 +0100

    while loop

--- day 4/forloop_ex.py
+++ day 4/forloop_ex.py
@@ -4,3 +4,12 @@
 # @TODO ile jest liczb przystych ile jest liczb nieparzystych w zbiorze
 # @TODO od start do stop włącznie
 
+start= int(input('Podaj start\n'))
+stop= int(input('Podaj stop\n'))
+krok= int(input('Podaj krok\n'))
+odds=0
+for idx in range(start,stop+1,krok):
+    if idx % 2:
+        odds+=1
+
+print(odds)
