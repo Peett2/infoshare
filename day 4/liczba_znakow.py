a13096436c6713d311c8d78ee9d808ebb3007e3d
Peett2 <piotr@skotnicki.co>
2018-02-12 16:10:04 +0100

    while loop

--- day 4/liczba_znakow.py
+++ day 4/liczba_znakow.py
@@ -3,17 +3,23 @@
 # ilosc_liter = str(len("Tomek36"))
 # print(ilosc_liter)
 
-string = input('Type in any string:\n')
+string = input('Wpisz dowolny ciąg znaków:\n')
+
+while len(string) < 1:
+    string = input('Musi być co najmniej jeden znak. '
+                   'Wpisz dowolny ciąg jeszcze raz:\n')
+
 digits = 0
 letters = 0
 others = 0
 
 for char in string:
     if char.isdigit():
-        digits+=1
+        digits += 1
     elif char.isalpha():
-        letters +=1
+        letters += 1
     else:
-        others +=1
+        others += 1
 
-print('Ciąg znaków posiada',digits,'cyfr,',letters,'liter oraz',others,'innych znaków')
+print('\nCiąg znaków posiada:\n', digits, 'cyfr,\n', letters,
+      'liter oraz\n', others, 'innych znaków')
