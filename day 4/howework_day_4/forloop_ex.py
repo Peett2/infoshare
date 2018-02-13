e1b197ff71cbf2f01d63538659a115a4b5eb4eed
Peett2 <piotr@skotnicki.co>
2018-02-13 11:05:23 +0100

    complete

--- day 4/howework_day_4/forloop_ex.py
+++ day 4/howework_day_4/forloop_ex.py
@@ -4,11 +4,12 @@
 #  ile jest liczb przystych ile jest liczb nieparzystych w zbiorze
 #  od start do stop włącznie
 
-x=input('Podaj dane w formacie: <start,stop,krok>:\n')
+x = input('Podaj dane w formacie: start,stop,krok:\n')
 
-start,stop,krok = x.split(',')
+start, stop, krok = x.split(',')
 
-print('Podane dane to:\n', start, stop, krok)
+print('Podane dane to:\n- start:', start, '\n- stop:',
+      stop, '\n- krok:', krok, '\n')
 
 start = int(start)
 stop = int(stop)
@@ -17,13 +18,13 @@ krok = int(krok)
 evens = 0
 odds = 0
 
-for idx in range(start, stop+1, krok):
+for idx in range(start, stop + 1, krok):
     if idx % 2:
         evens += 1
-        print(idx,';',evens,'evens')
+        print(idx, ';', evens, 'evens')
     elif idx + 1 % 2:
         odds += 1
-        print(idx,';',odds,'odds')
+        print(idx, ';', odds, 'odds')
 
-print('odds: ',odds)
-print('evens: ',evens)
+print('Odds:', odds)
+print('Evens:', evens)
